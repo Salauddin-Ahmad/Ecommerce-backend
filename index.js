b81ed3c2eb57424b9c9f364d53967be24053984c
Md. Salauddin <salauddinahmed555@gmail.com>
2024-08-18 15:14:24 +0600

    project started

---
+++ index.js
@@ -0,0 +1,89 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+
+const port = process.env.PORT || 5000;
+const corsOptions = {
+  origin: [
+    "*",
+    "http://localhost:5173",
+    "http://localhost:5174",
+  ],
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+
+app.use(express.json());
+
+
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.iu3pv7s.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+
+    const userCollection = client.db("Ecommerce").collection("users");
+
+    const productsCollection = client.db("Ecommerce").collection("products");
+    
+
+
+
+
+    // save the  user on db
+    app.post("/users", async (req, res) => {
+        const user = req.body;
+        // insert email if user doesn't exist
+        // you can do this many ways (1. email unique, 2. upsert
+        // 3. simple checking
+  
+        const query = { email: user.email };
+        const existingUser = await userCollection.findOne(query);
+        if (existingUser) {
+          return res.send({ message: "user already exists", insertedId: null });
+        }
+        const result = await userCollection.insertOne(user);
+        res.send(result);
+      });
+  
+
+
+
+
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get("/", (req, res) => {
+    res.send("Ecommerece Server is running ");
+  });
+  
+  app.listen(port, () => {
+    console.log(`Ecommerce Server is sitting on port ${port}`);
+  });
+  
